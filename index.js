0d5a6b63005c12ae8444516be75767e672a58f17
Bernard Pagoaga <bernard.pagoaga@webdealauto.com>
2018-02-23 17:33:40 +0100

    [UPDATE] Set up the tests

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello')
+const DIGITS = 1323465431;
+const EXAMPLES = [
+    1122,
+    1111,
+    1234,
+    91212129
+];
+
+const EXAMPLE_ANSWERS = [
+    3,
+    4,
+    0,
+    9
+]
+
+EXAMPLES.map((ex, i) => test(ex, i))
+
+function test(ex, i) {
+    const answer = solveCaptcha(ex);
+    console.log(answer === EXAMPLE_ANSWERS[i] ? 'pass' : 'fail')
+}
+
+function solveCaptcha(int) {
+    // the algo to solve the captach is here
+    result = 0
+    return result;
+}
